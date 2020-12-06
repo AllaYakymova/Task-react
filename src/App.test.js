b563e1ed1861d06e83e493195eb943ccad974ce6
Allegra <allochkaja112@gmail.com>
2020-12-06 22:27:07 +0200

    task redirect ready

--- src/App.test.js
+++
@@ -1,7 +0,0 @@
-
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getBy(<button/>);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
--- src/routers/AppRouters.js
+++ src/routers/AppRouters.js
@@ -1,15 +1,14 @@
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import EmployeesPage from '../pages/EmployeesPage/EmployeesPage'
-import EntrancePage from '../pages/EntrancePage/EntrancePage'
 import React from 'react'
 
 const AppRoutes = () => {
   return (
     <Switch>
-      <Route exact path="/" component={EntrancePage} />
+      <Redirect exact from='/' to='/employees' />
       <Route exact path="/employees" component={EmployeesPage} />
     </Switch>
   )
-}
+};
 
 export default AppRoutes
